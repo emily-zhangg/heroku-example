cf07877fccd7ada5fd2b24941aaaea91b0f3079c
emily-zhangg <emily@officecleanz.com>
2021-06-09 09:42:08 +0800

    updated postgres

--- index.js
+++ index.js
@@ -42,5 +42,22 @@ app.get("/bananas", (request, response) => {
 
   response.render("bananas", data);
 });
+app.get("/cats", (request, response) => {
+  console.log("request came in");
 
+  const whenDoneWithQuery = (error, result) => {
+    if (error) {
+      console.log("Error executing query", error.stack);
+      response.status(503).send(result.rows);
+      return;
+    }
+
+    console.log(result.rows[0].name);
+
+    response.send(result.rows);
+  };
+
+  // Query using pg.Pool instead of pg.Client
+  pool.query("SELECT * from cats", whenDoneWithQuery);
+});
 app.listen(PORT);
